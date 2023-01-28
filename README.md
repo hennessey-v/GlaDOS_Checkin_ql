1382395ebdfde65844a7602d97f287b6e639bb5c
hennessey-v <2949868968@qq.com>
2023-01-28 10:07:40 +0800

    :tada:1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# GlaDOS_Checkin_ql
+# GLaDOS 自动签到⚡
+
+- 基于 [Python](https://www.python.org/)语言
+- 可自定义签到时间（crontab）
+- 支持多账户
+- 支持部署在[青龙面板](https://github.com/whyour/qinglong)
+- 支持多种通知推送方式
+- [更新日志](#更新日志)
+
+## 简单介绍一下GLaDOS
+
+>GLaDOS是一家高速稳定的V2Ray/Trojan机场，超过4年的老品牌，官方网站使用自主开发的管理系统，属于技术派的老站，支持WireGuard协议加速，而且流量套餐性价比非常高。
+
+GLaDOS家的优惠活动非常良心，新账号注册一段时间后基本都会赠送一次30天的基础套餐激活码，如果你的账户是教育版edu账户，甚至可以直接得到365天的基础套餐。不仅如此，每天签到也会获得一天当前套餐的续期，因此才有了本项目。
+
+[GLaDOS 项目地址](https://github.com/glados-network/GLaDOS)
+
+## 使用说明
+
+### 一、准备工作
+
+- 账号的 cookie（并非仅此单一获取方式）
+
+  1. 注册 GLaDos 并登陆，在首页往下拉，找到 **我的会员 > 会员签到**
+
+     ![checkin_entrance](assets/checkin_entrance.png)
+
+  2. 点击跳转到签到页面
+
+     ![checkin_page](assets/checkin_page.png)
+
+  3. 打开 "开发者工具"，通常快捷键为 **F12**，或是点击 **浏览器选项 > 更多工具 > 开发者工具**，打开后如图所示点击 "**network**" 标签
+
+     ![devtools](assets/devtools.png)
+
+  4. 在签到页面点击签到，相对应的开发者工具 **network** 标签下会出现 "**checkin**" 请求，点击该请求，会出现更多信息，找到 "**Request Headers**" 里的 "**cookie**"，接下来设置密钥时需要用到
+
+     ![cookie](assets/cookie.png)
+## 二、在青龙面板中设置环境变量
+
+![cookie](assets/GR_cookie.png)
+- 多账号多次添加，运行脚本将自动遍历变量
+## 三、在青龙中拉取本仓库
+```
+ql repo https://github.com/hennessey-v/GlaDOS_Checkin_ql.git "checkin.py" "backUp|assets|README.md" "sendNotify.py"
+```
+## 四、运行脚本查看运行结果
+
+![cookie](assets/push_detail.png)
+
+## 更新日志
+
+### [1.0.0] - 2023-01-12
+
+项目发布
+
+#### 变更
+
+- 兼容多账户，推送信息增加账户邮箱信息提示
+
+#### 新增
+
+- 多账号签到
+- 多种推送渠道
+  - Bark服务
+  - TGBot推送
+  - QQ机器人
+  - 企业微信应用
+  - 企业微信BOT
+  - 微信推送Plus+
+
+
+## 鸣谢
+- 部分程序代码来源于开源项目[glados_checkin](https://github.com/akinlau/glados_checkin)，此外md文档风格及部分内容借鉴于[DullSword](https://github.com/DullSword)大佬
+
+
+## Star⭐
+
+**如果你觉得这个项目还不错的话，可以支持一下点个 Star⭐.**
---
+++ checkin.py
@@ -0,0 +1,86 @@
+import requests,json,os,sys,time
+
+
+# 在青龙面板环境变量添加GR_COOKIE，并登录https://glados.rocks，按f12--网络获取cookie
+def get_cookies():
+    CookieGRs = []
+    if os.environ.get("GR_COOKIE"):
+        print("已获取并使用Env环境 Cookie")
+        if '&' in os.environ["GR_COOKIE"]:
+            CookieGRs = os.environ["GR_COOKIE"].split('&')
+        elif '\n' in os.environ["GR_COOKIE"]:
+            CookieGRs = os.environ["GR_COOKIE"].split('\n')
+        else:
+            CookieGRs = [os.environ["GR_COOKIE"]]
+        # return CookieGRs
+    else:
+        print("未获取到正确✅格式的GlaDOS账号Cookie")
+        return
+
+    print(f"====================共{len(CookieGRs)}个GlaDOS账号Cookie=========\n")
+    print(f"==================脚本执行- 北京时间(UTC+8)：{time.strftime('%Y/%m/%d %H:%M:%S', time.localtime())}=====================\n")
+    return CookieGRs
+
+
+def load_send():
+    global send
+    cur_path = os.path.abspath(os.path.dirname(__file__))
+    sys.path.append(cur_path)
+    if os.path.exists(cur_path + "/sendNotify.py"):
+        try:
+            from sendNotify import send
+        except:
+            send=False
+            print("加载通知服务失败~")
+    else:
+        send=False
+        print("加载通知服务失败~")
+
+
+def checkin(e): 
+    cookie = e
+    checkin_url= "https://glados.rocks/api/user/checkin"
+    state_url= "https://glados.rocks/api/user/status"
+    referer = 'https://glados.rocks/console/checkin'
+    origin = "https://glados.rocks"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+    payload={
+        'token': 'glados.network'
+    }
+    try:
+        checkin = requests.post(checkin_url,headers={
+            'cookie': cookie ,
+            'referer': referer,
+            'origin':origin,
+            'user-agent':useragent,
+            'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
+        state =  requests.get(state_url,headers={
+            'cookie': cookie ,
+            'referer': referer,
+            'origin':origin,
+            'user-agent':useragent})
+    except Exception as e:
+        print(f"签到失败，请检查网络：{e}")
+    mess = checkin.json()['message']
+    mail = state.json()['data']['email']
+    time = state.json()['data']['leftDays']
+    time = time.split('.')[0]
+    return mess,time,mail
+
+def run_checkin():
+    tempcontent = tempcontents = ""
+    cookies = get_cookies()
+    for i in cookies:
+        cookie = i
+        ret,remain,email = checkin(cookie)
+        content = f"账号：{email}\n签到结果：{ret}\n剩余天数：{remain}\n\n"
+        tempcontent = tempcontents
+        tempcontents =  tempcontent + content
+    print(tempcontents)
+    return tempcontents
+
+if __name__ == '__main__':
+    title = "GlaDOS签到通知"
+    contents = run_checkin()
+    load_send()
+    send(title,contents)
